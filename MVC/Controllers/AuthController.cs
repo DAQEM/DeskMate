f00eecd12a0cb936ad61f2bad7a841213b043862
DAQEM <daqemyt@gmail.com>
2023-03-14 09:09:20 +0100

    Split the layouts in multiple partials.

---
+++ MVC/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models.Auth;
+
+namespace MVC.Controllers;
+
+public class AuthController : Controller
+{
+    [HttpGet]
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public IActionResult Login(LoginModel loginModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            //TODO: login
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    [Route("logout")]
+    public IActionResult Logout()
+    {
+        //TODO: logout
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    [Route("register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public IActionResult Register(RegisterModel registerModel)
+    {
+        if (!ModelState.IsValid)
+            //TODO: register
+            return RedirectToAction("Index", "Home");
+        {
+            return View();
+        }
+    }
+}
