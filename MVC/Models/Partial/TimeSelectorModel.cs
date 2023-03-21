4b47484aec6a53ef038cbcdbfeba2fb960084e55
DAQEM <daqemyt@gmail.com>
2023-03-21 15:55:31 +0100

    Improved date selection view.

---
+++ MVC/Models/Partial/TimeSelectorModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MVC.Models.Partial;
+
+public class TimeSelectorModel
+{
+    public string Type { get; set; }
+    public DateTime Date { get; set; }
+
+    public int Hours => Date.Hour;
+
+    public int Minutes
+    {
+        get
+        {
+            int hour = Date.Minute;
+            while (hour % 5 != 0)
+            {
+                hour++;
+            }
+
+            return hour;
+        }
+    }
+}
