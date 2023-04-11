25d4a5fbf798853c273f7b63f547d1bdfa22ebc9
DAQEM <daqemyt@gmail.com>
2023-04-11 15:26:59 +0200

    Changed Selection page to 1 page instead of a multiple submit page.

--- MVC/Controllers/SelectionController.cs
+++ MVC/Controllers/SelectionController.cs
@@ -6,6 +6,7 @@ using MVC.Models;
 
 namespace MVC.Controllers;
 
+[Route("Selection")]
 public class SelectionController : BaseController<SelectionController>
 {
     private readonly IFloorService _floorService;
@@ -21,83 +22,38 @@ public class SelectionController : BaseController<SelectionController>
     }
 
     [HttpGet]
+    [Route("")]
     public IActionResult Index()
     {
-        return View(new SelectionModel());
+        //TODO: Get the employee from the session
+        List<EmployeeModel> employeeModels =
+            new() { new EmployeeModel(new Employee(Guid.NewGuid(), "Me")) };
+
+        List<LocationModel> locationModels =
+            _locationService.GetAllLocations().Select(LocationModel.FromLocation).ToList();
+
+        List<FloorModel> floorModels =
+            _floorService.GetFloorsByLocationId(locationModels[0].Id).Select(FloorModel.FromFloor).ToList();
+
+        List<WorkspacePropModel> workspaceModels =
+            _workspaceService.GetWorkspacesByFloorId(floorModels[0].Id).Select(WorkspacePropModel.FromWorkspace)
+                .ToList();
+
+        SelectionModel model = new()
+        {
+            EmployeeModels = employeeModels,
+            LocationModels = locationModels,
+            FloorModels = floorModels,
+            WorkspaceModels = workspaceModels
+        };
+
+        return View(model);
     }
 
     [HttpPost]
+    [Route("")]
     public IActionResult Selection(SelectionModel model)
     {
-        Console.WriteLine(model.Step);
-        return model.Step switch
-        {
-            2 => DateTimeSelection(model),
-            3 => EmployeeSelection(model),
-            4 => LocationSelection(model),
-            5 => FloorSelection(model),
-            6 => WorkspaceSelection(model),
-            _ => Index()
-        };
-    }
-
-    [HttpPost]
-    public IActionResult DateTimeSelection(SelectionModel model)
-    {
-        Console.WriteLine(DateTime.Now.ToLongTimeString() + " => " +
-                          model.DateTimeSelectionModel.Date.ToShortDateString() + " " +
-                          model.DateTimeSelectionModel.StartTime.ToShortTimeString() + " " +
-                          model.DateTimeSelectionModel.EndTime.ToShortTimeString());
-        //TODO: Get employees from database
-        model.EmployeeModels = new List<EmployeeModel>
-            { new(new Employee(Guid.NewGuid(), "Viewer name")) };
-        return View("Index", model);
-    }
-
-    [HttpPost]
-    public IActionResult EmployeeSelection(SelectionModel model)
-    {
-        Console.WriteLine(DateTime.Now.ToLongTimeString() + " => " + model.EmployeeModels.Count);
-        model.LocationModels = _locationService.GetAllLocations().Select(LocationModel.FromLocation).ToList();
-        return View("Index", model);
-    }
-
-    [HttpPost]
-    public IActionResult LocationSelection(SelectionModel model)
-    {
-        Console.WriteLine(DateTime.Now.ToLongTimeString() + " => " + model.LocationModels.Count);
-        model.LocationModels = _locationService.GetAllLocations().Select(LocationModel.FromLocation).ToList();
-        model.FloorModels = _floorService.GetFloorsByLocationId(model.SelectedLocationId)
-            .Select(FloorModel.FromFloor).ToList();
-        return View("Index", model);
-    }
-
-    [HttpPost]
-    public IActionResult FloorSelection(SelectionModel model)
-    {
-        Console.WriteLine(DateTime.Now.ToLongTimeString() + " => " + model.FloorModels.Count);
-        model.LocationModels = _locationService.GetAllLocations().Select(LocationModel.FromLocation).ToList();
-        model.FloorModels = _floorService.GetFloorsByLocationId(model.SelectedLocationId)
-            .Select(FloorModel.FromFloor).ToList();
-        model.WorkspaceModels = _workspaceService.GetWorkspacesByFloorId(model.SelectedFloorId)
-            .Select(WorkspacePropModel.FromWorkspace).ToList();
-        return View("Index", model);
-    }
-
-    [HttpPost]
-    public IActionResult WorkspaceSelection(SelectionModel model)
-    {
-        Console.WriteLine(DateTime.Now.ToLongTimeString() + " => " + model.WorkspaceModels.Count);
-        foreach (KeyValuePair<string, string> keyValuePair in model.Reservations)
-        {
-            Console.WriteLine("Reservation: " + keyValuePair.Key + " " + keyValuePair.Value);
-        }
-
-        model.LocationModels = _locationService.GetAllLocations().Select(LocationModel.FromLocation).ToList();
-        model.FloorModels = _floorService.GetFloorsByLocationId(model.SelectedLocationId)
-            .Select(FloorModel.FromFloor).ToList();
-        model.WorkspaceModels = _workspaceService.GetWorkspacesByFloorId(model.SelectedFloorId)
-            .Select(WorkspacePropModel.FromWorkspace).ToList();
         return View("Index", model);
     }
 }
